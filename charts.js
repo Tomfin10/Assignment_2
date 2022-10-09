8ee0e7470ff5540133daf9b10f7dd27d87173d97
Tomfin10 <61998604+Tomfin10@users.noreply.github.com>
2022-10-09 11:23:08 +1100

    moving assignment 2

---
+++ charts.js
@@ -0,0 +1,29 @@
+var map_vg_1 = "victims_map.json";
+vegaEmbed("#map", map_vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var death_time_vg = "death_time2.json";
+vegaEmbed("#death_time", death_time_vg).then(function(result) {
+
+}).catch(console.error);
+
+var state_rank_vg = "state_rank.json";
+vegaEmbed("#state_rank", state_rank_vg).then(function(result) {
+
+}).catch(console.error);
+
+var map_killed_state_vg = "map_killed_states.json";
+vegaEmbed("#map_killed_state", map_killed_state_vg).then(function(result) {
+
+}).catch(console.error);
+
+var killed_donut_vg = "killed_donut.json";
+vegaEmbed("#killed_donut", killed_donut_vg).then(function(result) {
+
+}).catch(console.error);
+
+var injured_donut_vg = "injured_donut.json";
+vegaEmbed("#injured_donut", injured_donut_vg).then(function(result) {
+
+}).catch(console.error);
