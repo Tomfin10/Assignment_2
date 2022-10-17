ca89eb98b850c91d0c4a5b14b671802d1c218b9e
Tomfin10 <61998604+Tomfin10@users.noreply.github.com>
2022-10-17 15:42:29 +1100

    css_edit

--- charts.js
+++ charts.js
@@ -1,34 +1,34 @@
 var map_killed_state_vg = "map_killed_states.json";
-vegaEmbed("#map_killed_state", map_killed_state_vg).then(function(result) {
+vegaEmbed("#map_killed_state", map_killed_state_vg, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var killed_donut_vg = "killed_donut.json";
-vegaEmbed("#killed_donut", killed_donut_vg).then(function(result) {
+vegaEmbed("#killed_donut", killed_donut_vg, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var injured_donut_vg = "injured_donut.json";
-vegaEmbed("#injured_donut", injured_donut_vg).then(function(result) {
+vegaEmbed("#injured_donut", injured_donut_vg, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var casualties_donut_vg = "casualties_donut.json";
-vegaEmbed("#casualties_donut", casualties_donut_vg).then(function(result) {
+vegaEmbed("#casualties_donut", casualties_donut_vg, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var attack_type_vg = "attack_type.json";
-vegaEmbed("#attack_type", attack_type_vg).then(function(result) {
+vegaEmbed("#attack_type", attack_type_vg, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var targ_indis_vg = "targ_indis.json";
-vegaEmbed("#targ_indis", targ_indis_vg).then(function(result) {
+vegaEmbed("#targ_indis", targ_indis_vg, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var day_time_vg = "day_time.json";
-vegaEmbed("#day_time", day_time_vg).then(function(result) {
+vegaEmbed("#day_time", day_time_vg, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
